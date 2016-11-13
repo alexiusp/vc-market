a1c8067b66669fc10b41815bc02e4e31c272e898
Aleksei "AlexiuS" Podgaev <podgaev@gmail.com>
2016-11-13 20:51:01 +0100

    deploying angular app

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 
 var app = require('./srv/app');
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
+app.use('/app', express.static(__dirname + '/app'));
 // request logging function
 app.use(function(req, res, next) {
   console.log('%s %s %s', req.method, req.url, req.path);
