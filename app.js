5d14b64c4a99d83b25ca9ccb51e08528a08a6cce
Aleksei "AlexiuS" Podgaev <podgaev@gmail.com>
2016-11-13 22:33:37 +0100

    base href and static routing fix

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var express = require('express');
 
 var app = require('./srv/app');
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
-app.use('/app', express.static(__dirname + '/app'));
+app.use('/', express.static(__dirname + '/app'));
+/*
 // request logging function
 app.use(function(req, res, next) {
   console.log('%s %s %s', req.method, req.url, req.path);
   next();
 });
+*/
